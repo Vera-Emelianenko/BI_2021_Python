ac3a34ad46c426a0f7282a1a7f3f6c1d477e3e26
Vera Emelianenko <emelianenko.vera@gmail.com>
2021-10-04 22:18:43 +0300

    edited punctuation in simple unit converter

--- units_converter.py
+++ units_converter.py
@@ -1,15 +1,16 @@
 help_message = '''
-This program converts some volume units to cubic metre. 
+This program converts some volume units to cubic metre.
 '''
 invalid_unit_message = '''
 Could not recognise units.
 Try using 'dm3', 'cm3', 'mm3', 'L', 'mL', 'yd3', 'ft3', 'in3'
 '''
-valid_units_dict = {'dm3':0.001, 'cm3':0.001*0.001, 'mm3':0.001*0.001*0.001,
-'L':0.001, 'mL':0.001*0.001,
-'yd3': 0.764554857984, 'ft3': 0.001*28.316846592, 'in3':16.387064*0.001*0.001,
-'bl': 158.987295*0.001, 'pt': 0.473176473*0.001, 'oz':29.573531*0.001*0.001
-}
+valid_units_dict = {'dm3': 0.001, 'cm3': 0.001*0.001, 'mm3': 0.001*0.001*0.001,
+    'L': 0.001, 'mL': 0.001*0.001,
+    'yd3': 0.764554857984, 'ft3': 0.001*28.316846592, 'in3': 16.387064*0.001*0.001,
+    'bl': 158.987295*0.001, 'pt': 0.473176473*0.001, 'oz': 29.573531*0.001*0.001
+                    }
+
 
 # define main function
 def main():
@@ -40,7 +41,7 @@ def main():
 
         else:
             number_in_metres = valid_units_dict[command.split()[1]]*float(command.split()[0])
-            print (str (number_in_metres) + 'm3')
+            print(str(number_in_metres) + 'm3')
 
 
 # call main function so it's executed
