b613a30e7ba0a9ee121356cbbaced15862b3a090
Vera Emelianenko <emelianenko.vera@gmail.com>
2021-10-04 22:13:22 +0300

    edited simple unit converter

--- units_converter.py
+++ units_converter.py
@@ -2,9 +2,14 @@ help_message = '''
 This program converts some volume units to cubic metre. 
 '''
 invalid_unit_message = '''
-Invalid units. Try using 'dm3', 'cm3', 'mm3', 'L', 'mL'
+Could not recognise units.
+Try using 'dm3', 'cm3', 'mm3', 'L', 'mL', 'yd3', 'ft3', 'in3'
 '''
-valid_units_dict = {'dm3':0.001, 'cm3':0.001*0.001, 'mm3':0.001*0.001*0.001, 'L':0.001, 'mL':0.001*0.001}
+valid_units_dict = {'dm3':0.001, 'cm3':0.001*0.001, 'mm3':0.001*0.001*0.001,
+'L':0.001, 'mL':0.001*0.001,
+'yd3': 0.764554857984, 'ft3': 0.001*28.316846592, 'in3':16.387064*0.001*0.001,
+'bl': 158.987295*0.001, 'pt': 0.473176473*0.001, 'oz':29.573531*0.001*0.001
+}
 
 # define main function
 def main():
