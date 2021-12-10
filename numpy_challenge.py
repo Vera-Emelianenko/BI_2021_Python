6945da9c3f435628a56cd8ff3b5a75f76653ea15
Vera Emelianenko <emelianenko.vera@gmail.com>
2021-12-10 16:21:24 +0300

    corrected for flake8

--- numpy_challenge.py
+++ numpy_challenge.py
@@ -1,15 +1,15 @@
 import numpy as np
 if __name__ == "__main__":
-# create 3 arrays in np
-# first array - from python object
+    # create 3 arrays in np
+    # first array - from python object
     array1 = np.array([1.5, 2.1, 3.6, 4.8, 5.5, 6.1, 7.0, 8.4, 9.2, 10.9])
-# second array - using .full function 
+    # second array - using .full function 
     array2 = np.full((15, 10), 0)
-# third array  - using arrange
+    # third array  - using arrange
     array3 = np.arange(0, 100, 2.5)
 
 
-def matrix_multiplication1(a1,a2):
+def matrix_multiplication1(a1, a2):
     """Multiplies two matrices
 
     Parameters:
@@ -21,14 +21,14 @@ def matrix_multiplication1(a1,a2):
     if a1.shape[1] == a2.shape[0]:
         result = np.zeros((a1.shape[0], a2.shape[1]))
         for i in range(a1.shape[1]):
-            for j in range (a2.shape[0]):
-                result[i,j] = sum(a1[i,:]*a2[:,j])
+            for j in range(a2.shape[0]):
+                result[i, j] = sum(a1[i, :]*a2[:, j])
         return result
     else:
         return None
 
 
-def matrix_multiplication(a1,a2):
+def matrix_multiplication(a1, a2):
     """Multiplies two matrices using numpy.matmul() function
 
     Parameters:
@@ -38,7 +38,7 @@ def matrix_multiplication(a1,a2):
     numpy array if matrices can be multiplied, None if they can't
     """
     if a1.shape[1] == a2.shape[0]:
-        result = np.matmul(a1,a2)
+        result = np.matmul(a1, a2)
         return result
     else:
         return None
@@ -77,11 +77,12 @@ def multiply_matrices(list_of_matrices):
         return None
 
 
-def compute_2d_distance(a1,a2):
+def compute_2d_distance(a1, a2):
     """Calculates distance between 2 points in 2 dimensions
+
     Parameters:
     a1, a2 - numpy arrays of shape (2,), a1 = ([x1,y1]), a2 = ([x2,y2])
-    
+
     Returns:
     float: 2d distance between 2 points
     """
