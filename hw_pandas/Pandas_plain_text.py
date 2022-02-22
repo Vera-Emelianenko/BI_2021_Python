102c3137e4aa645f21a26a9ef4cb007472e6ad97
Vera Emelianenko <emelianenko.vera@gmail.com>
2022-02-22 17:47:17 +0300

    flake8

--- hw_pandas/Pandas_plain_text.py
+++ hw_pandas/Pandas_plain_text.py
@@ -1,7 +1,6 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
-%matplotlib inline
 import numpy as np
 
 train_df = pd.read_csv("https://raw.githubusercontent.com/Serfentum/bf_course/master/14.pandas/train.csv")
