fbbdb7609aac74d99f161ba4b0ee21565ec3c757
Vera Emelianenko <emelianenko.vera@gmail.com>
2022-02-22 17:52:37 +0300

    flake8

--- hw_pandas/Pandas_plain_text.py
+++ hw_pandas/Pandas_plain_text.py
@@ -1,14 +1,14 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
-import numpy as np
+import pandas_profiling as pp
 
 train_df = pd.read_csv("https://raw.githubusercontent.com/Serfentum/bf_course/master/14.pandas/train.csv")
 
-sns.displot(train_df , x="A")
-sns.displot(train_df , x="T")
-sns.displot(train_df , x="G")
-sns.displot(train_df , x="C")
+sns.displot(train_df, x="A")
+sns.displot(train_df, x="T")
+sns.displot(train_df, x="G")
+sns.displot(train_df, x="C")
 
 
 for nucleotide in ['A', 'T', 'G', 'C']:
@@ -19,10 +19,11 @@ plt.xlabel('number of contigs with this nucleotide')
 plt.ylabel('count')
 plt.show()
 
-train_part = train_df.loc[:, ['pos', 'reads_all', 'mismatches', 'insertions']][train_df.mismatches > train_df.mismatches.mean()]
+train_part = train_df.loc[:,
+    ['pos', 'reads_all', 'mismatches', 'insertions']][train_df.mismatches > train_df.mismatches.mean()]
 train_part.to_csv('train_part.csv', index=False)
 
-yersinia = pd.read_csv("./Chromosomes_full.tsv", sep = '\t')
+yersinia = pd.read_csv("./Chromosomes_full.tsv", sep='\t')
 
 
 yersinia = yersinia.iloc[:, 1:]
@@ -33,27 +34,28 @@ yersinia.describe()
 yersinia.info()
 
 yersinia['rec.shape'].unique()
-yersinia['rec.type'].unique() 
-yersinia['species'].unique() 
+yersinia['rec.type'].unique()
+yersinia['species'].unique()
 
 yersinia.columns
 
-f, axes = plt.subplots(figsize=(60,5), nrows=1, ncols=11)
-features = ['rec.length', 'rec.proteins','species', 'minGC', 'maxGC','length_replichore1', 'length_replichore2', 'ratio', 'genome_size',
-       'disbalance', 'GCcontent']
-for feature in range (11):
+f, axes = plt.subplots(figsize=(60, 5), nrows=1, ncols=11)
+features = ['rec.length', 'rec.proteins', 'species', 'minGC', 'maxGC',
+            'length_replichore1', 'length_replichore2', 'ratio', 'genome_size',
+            'disbalance', 'GCcontent']
+
+for feature in range(11):
     axes_number = axes[feature]
     sns.histplot(x=yersinia[features[feature]], ax=axes_number)
 
 
-f, axes = plt.subplots(figsize=(60,60), nrows=11, ncols=11)
-features = ['rec.length', 'rec.proteins','species', 'minGC', 'maxGC','length_replichore1', 'length_replichore2', 'ratio', 'genome_size',
-       'disbalance', 'GCcontent']
+f, axes = plt.subplots(figsize=(60, 60), nrows=11, ncols=11)
+
 used_features = []
-for feature in range (11):
-    for feature2 in range (11):
+for feature in range(11):
+    for feature2 in range(11):
         axes_number = axes[feature, feature2]
-        if feature == feature2: 
+        if feature == feature2:
             sns.histplot(x=yersinia[features[feature]], ax=axes_number)
         else: 
             sns.scatterplot(data=yersinia, y=features[feature], x=features[feature2], ax=axes_number)
@@ -62,6 +64,4 @@ for feature in range (11):
 corr = yersinia.corr(method='pearson')
 corr.style.background_gradient(cmap='coolwarm')
 
-
-import pandas_profiling as pp
 pp.ProfileReport(yersinia)
