c8c24ed91c2cf36c080d21bbb159b4f9c2fde0cb
Vera Emelianenko <emelianenko.vera@gmail.com>
2022-03-29 13:04:32 +0300

    flake8

--- hw_iterators/hw_iterators.py
+++ hw_iterators/hw_iterators.py
@@ -1,8 +1,9 @@
-# Iterators homework 
+# Iterators homework
 
 import random
 
-## Task 1
+# Task 1
+
 
 def fasta_iterator(path: str):
     """
@@ -20,6 +21,7 @@ def fasta_iterator(path: str):
                 seq = ('').join(entry.split('\n')[1:])
                 yield '> ' + seq_id, seq
 
+
 # initialize a generator
 reader = fasta_iterator('./sequences.fasta')
 print(type(reader))
@@ -30,6 +32,7 @@ for id_, seq in reader:
 
 ## Task 2
 
+
 class MutatedFastaIterator:
     """
     A class used to iterate over fasta sequences from a given file
@@ -39,7 +42,7 @@ class MutatedFastaIterator:
     Generates sequences with 'mutations':
        - random aminoacid is changed for glycin
        - deletion up to 5 aminoacids is introduced
-       
+
     Attributes
     ----------
     path : str
@@ -55,7 +58,7 @@ class MutatedFastaIterator:
         Prints the next mutated sequence
     """
 
-    def __init__ (self, path):
+    def __init__(self, path):
         """
         Initializes the object of the class. Accepts path to fasta file.
         """
@@ -79,7 +82,7 @@ class MutatedFastaIterator:
         seq = ('').join(entry.split('\n')[1:])
         seq_length = len(seq)
         # generate random index from 0 to seq_length-6
-        random_index=random.randrange(seq_length-6)
+        random_index=random.randrange(seq_length - 6)
         # generate number of aminoacid that we want to remove, up to 5
         number_of_aa_deleted = random.randrange(5)
         # delete random number of aa (up to 5) from position random_index
@@ -90,7 +93,6 @@ class MutatedFastaIterator:
         # join sequence id and sequence
         new_entry = '>' + seq_id + '\n' + seq_with_mutation_to_glycin + '\n'
         return new_entry
-
 
     def __next__(self):
         """
@@ -107,6 +109,7 @@ class MutatedFastaIterator:
         seq = self.sequences[self.__cur_index]
         new_seq = self.changed_seq(seq)
         return new_seq
+
 
 # initialize an object of class MutatedFastaIterator
 sequences_fasta_iterator = (MutatedFastaIterator('./sequences.fasta'))
